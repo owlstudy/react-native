9ca2273a42cfb8ac070685bb132265e71e1afa8d
hyunjin <leehj0110@kakao.com>
2022-01-14 14:13:17 +0900

    feat: init rn project

---
+++ tinder/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+   <View>
+     <Text style={{textAlign: 'center', margin:50}}>hi</Text>
+   </View>
+  )
+}
+
+export default App;

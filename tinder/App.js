2922436734ba62c5a4819abfaf8391cb35b8b416
hyunjin <leehj0110@kakao.com>
2022-01-14 14:44:00 +0900

    feat: thinder card UI

--- tinder/App.js
+++ tinder/App.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native';
 
 const App = () => {
   return (
-   <View>
-     <Text style={{textAlign: 'center', margin:50}}>hi</Text>
-   </View>
-  )
-}
+    <View style={styles.pageContainer}>
+      <View style={styles.card}>
+        <ImageBackground
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+          }}
+          style={styles.image}
+        >
+          <View style={styles.cardInner}>
+            <Text style={styles.name}>Elon Musk</Text>
+            <Text style={styles.bio}>zzdddz</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  bio: {
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 25,
+  },
+});
 
 export default App;
